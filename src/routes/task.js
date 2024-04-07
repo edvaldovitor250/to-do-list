03e05847ee215330f7f637e92a12e0f1d603f9c6
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-04-07 18:05:22 -0300

    fix: organizing the src folder

--- src/routes/task.js
+++ src/routes/task.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const router = express.Router;
 const Checklist = require('../models/checklist');
 const Task = require('../models/task');
 const checklistDependentRoute = express.Router();
